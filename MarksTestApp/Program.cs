7a55d0242662af1ef09335b3635257dc985d317f
Wendell <jack0817@yahoo.com>
2016-06-23 21:40:17 -0400

    Adding baddassness

--- MarksTestApp/Program.cs
+++ MarksTestApp/Program.cs
@@ -36,68 +36,91 @@ namespace MarksTestApp
                 Console.WriteLine(age);
             }
 
-          
-            //if (ismarkolder && whiteness)
-            //{
-            //    Console.WriteLine("valid");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("invalid");
-            //}
+            int[] values = { 4, 4, 4, 4, 3, 1234, 323, 1234123, 1234, 1234, 1, 2, 2, 3, 3, 3 };
+            int[] printedValues = { };
 
-            //switch (dannyage)
-            //{
-            //    case 40:
-            //        Console.WriteLine("Danny is 38");
-            //        break;
-            //    default :
-            //        Console.WriteLine("DANNY IS UNKNOWN");
-            //        break;
+            foreach (int v in values)
+            {
+                bool isFound = false;
+                for (int i = 0; i < printedValues.Length; i++)
+                {
+                    if (printedValues[i] == v)
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
 
-            //}
-
-            //for (int x = ages.Length - 1; x >= 0; x--)
-            //{
-            //    int ageValue = ages[x];
-            //    Console.WriteLine(ageValue);
-            //}
-
-            //bool keepgoing = true;
-            //int current = 0;
-            //while (keepgoing)
-            //{
-            //    current += 1;
-            //    if (current == 100001)
-            //    {
-            //        keepgoing = false;
-            //        Console.WriteLine("WOOT");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(current);
-            //    }
-
-            //}
+                if (!isFound)
+                {
+                    Console.WriteLine("Distinct Value:{0}", v);
+                    int[] newArray = new int[printedValues.Length + 1];
+                    printedValues.CopyTo(newArray, 0);
+                    newArray[newArray.Length - 1] = v;
+                    printedValues = newArray;
 
 
+                    //if (ismarkolder && whiteness)
+                    //{
+                    //    Console.WriteLine("valid");
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("invalid");
+                    //}
 
-            //Console.WriteLine("{0}", isvalid);
+                    //switch (dannyage)
+                    //{
+                    //    case 40:
+                    //        Console.WriteLine("Danny is 38");
+                    //        break;
+                    //    default :
+                    //        Console.WriteLine("DANNY IS UNKNOWN");
+                    //        break;
 
-            //Person Mark = new Person();
-            //Mark.Age = 12;
-            //Mark.BirthDate = DateTime.Now;
+                    //}
 
-            //Person Danny = new Person();
-            //Danny.Age = 13;
-            //Danny.BirthDate = DateTime.Now;
+                    //for (int x = ages.Length - 1; x >= 0; x--)
+                    //{
+                    //    int ageValue = ages[x];
+                    //    Console.WriteLine(ageValue);
+                    //}
 
-            //int NewAge = Mark.Age + Danny.Age;
+                    //bool keepgoing = true;
+                    //int current = 0;
+                    //while (keepgoing)
+                    //{
+                    //    current += 1;
+                    //    if (current == 100001)
+                    //    {
+                    //        keepgoing = false;
+                    //        Console.WriteLine("WOOT");
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine(current);
+                    //    }
 
-            //Console.WriteLine(NewAge);
+                    //}
 
 
-            Console.Read();
+
+                    //Console.WriteLine("{0}", isvalid);
+
+                    //Person Mark = new Person();
+                    //Mark.Age = 12;
+                    //Mark.BirthDate = DateTime.Now;
+
+                    //Person Danny = new Person();
+                    //Danny.Age = 13;
+                    //Danny.BirthDate = DateTime.Now;
+
+                    //int NewAge = Mark.Age + Danny.Age;
+
+                    //Console.WriteLine(NewAge);
+
+
+                    Console.Read();
 
 
             
