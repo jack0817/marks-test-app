0c3bdb1f048ec2ac000a61b0d5164b2055537f84
Danny <opiatezeo@gmail.com>
2016-06-30 22:53:07 -0400

    Danny's stuff

--- MarksTestApp/Danny.cs
+++ MarksTestApp/Danny.cs
@@ -10,38 +10,44 @@ namespace MarksTestApp
     {
         public static void Test()
         {
-            while (true)
-            {
-                Console.WriteLine("Type x to Exit");
-                var input = Console.ReadLine();
-                if (input == "x")
-                {
-                    break;
-                }
-            }
+            //while (true)
+            //{
+            //    Console.WriteLine("Type x to Exit");
+            //    var input = Console.ReadLine();
+            //    if (input == "x")
+            //    {
+            //        break;
+            //    }
+            //}
 
             //List<int> columns = new List<int>(8);
             //List<int> rows = new List<int>(8);
-            var checkerboard = new List<List<int>>(64);
-            int maxColumns = 8;
-            int maxRows = 8;
 
-            for (int c = 0; c < maxColumns; c++)
-            {
-                checkerboard.Add(new List<int>());
-                for (int r = 0; r < maxRows; r++)
-                {
-                    checkerboard[c].Add(r);
-                }
-            }
 
-            for (int c = 0; c < maxColumns; c++)
-            {
-                for (int r = 0; r < maxRows; r++)
-                {
-                    Console.WriteLine("{0}", checkerboard[c][r]);
-                }
-            }
+
+            //var checkerboard = new List<List<int>>(64);
+            //int maxColumns = 8;
+            //int maxRows = 8;
+
+            //for (int c = 0; c < maxColumns; c++)
+            //{
+            //    checkerboard.Add(new List<int>());
+            //    for (int r = 0; r < maxRows; r++)
+            //    {
+            //        checkerboard[c].Add(r);
+            //    }
+            //}
+
+            //for (int c = 0; c < maxColumns; c++)
+            //{
+            //    for (int r = 0; r < maxRows; r++)
+            //    {
+            //        Console.WriteLine("{0}", checkerboard[c][r]);
+            //    }
+            //}
+
+
+
 
             //foreach (int c in columns)
             //{
@@ -51,7 +57,81 @@ namespace MarksTestApp
             //    }
             //}
 
+            var dannyplane = new List<List<bool>>();
+            int planeColumns = 4;
+            int planeRows = 10;
 
+            for (int c = 0; c < planeColumns; c++)
+            {
+                var column = new List<bool>();
+                dannyplane.Add(column);
+                for (int row = 0; row < planeRows; row++)
+                {
+                    column.Add(false);
+                    //dannyplane[c].Add(row);
+                }
+            }
+
+            //for (int rows = 0; rows < planeRows; rows++)
+            //{
+            //    for (int c = 0; c < planeColumns; c++)
+            //    {
+            //        Console.Write("[{0}] ", dannyplane[c][rows]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            while (true)
+            {
+                for (int rows = 0; rows < planeRows; rows++)
+                {
+                    for (int c = 0; c < planeColumns; c++)
+                    {
+                        Console.Write("[{0}] ", dannyplane[c][rows]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Do you want a Seat? Press Y for yes, or X to exit the application");
+                var input = Console.ReadLine();
+                if (input == "x" || input == "X")
+                {
+                    break;
+                }
+                else if (input.Equals("y", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    bool isfound = false;
+                    for (int rows = 0; rows < planeRows && !isfound; rows++)
+                    {
+                        for (int c = 0; c < planeColumns; c++)
+                        {
+                            if (dannyplane[c][rows] == false)
+                            {
+                                isfound = true;
+                                dannyplane[c][rows] = true;
+                                Console.WriteLine("Seat Found at column [{0}] row [{1}]!", c, rows);
+                                break;
+                            }
+
+                        }
+
+                        //    if (isfound)
+                        //    {
+                        //        break;
+                        //    }
+                    }
+
+                    if (isfound == false)
+                    {
+                        Console.WriteLine("TAKEOFF!!!!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry!!!!");
+                }
+            }
 
         }
     }
--- MarksTestApp/Program.cs
+++ MarksTestApp/Program.cs
@@ -26,108 +26,111 @@ namespace MarksTestApp
             //int[] ages = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 };
             //Console.WriteLine(ages);
 
-            List<int> ages = new List<int> { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 };
+            //List<int> ages = new List<int> { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 };
 
-            IEnumerable<int> distinctAges = ages.Distinct();
+            //IEnumerable<int> distinctAges = ages.Distinct();
 
-            Console.WriteLine("Distinct ages:");
+            //Console.WriteLine("Distinct ages:");
 
-            foreach (int age in distinctAges)
-            {
-                Console.WriteLine(age);
-            }
+            //foreach (int age in distinctAges)
+            //{
+            //    Console.WriteLine(age);
+            //}
 
-            int[] values = { 4, 4, 4, 4, 3, 1234, 323, 1234123, 1234, 1234, 1, 2, 2, 3, 3, 3 };
+            //int[] values = { 4, 4, 4, 4, 3, 1234, 323, 1234123, 1234, 1234, 1, 2, 2, 3, 3, 3 };
 
-            int[] printedValues = { };
+            //int[] printedValues = { };
 
-            foreach (int v in values)
-            {
-                bool isFound = false;
-                for (int i = 0; i < printedValues.Length; i++)
-                {
-                    if (printedValues[i] == v)
-                    {
-                        isFound = true;
-                        break;
-                    }
-                }
+            //foreach (int v in values)
+            //{
+            //    bool isFound = false;
+            //    for (int i = 0; i < printedValues.Length; i++)
+            //    {
+            //        if (printedValues[i] == v)
+            //        {
+            //            isFound = true;
+            //            break;
+            //        }
+            //    }
 
-                if (!isFound)
-                {
-                    Console.WriteLine("Distinct Value:{0}", v);
-                    int[] newArray = new int[printedValues.Length + 1];
-                    printedValues.CopyTo(newArray, 0);
-                    newArray[newArray.Length - 1] = v;
-                    printedValues = newArray;
-
-
-                    //if (ismarkolder && whiteness)
-                    //{
-                    //    Console.WriteLine("valid");
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine("invalid");
-                    //}
-
-                    //switch (dannyage)
-                    //{
-                    //    case 40:
-                    //        Console.WriteLine("Danny is 38");
-                    //        break;
-                    //    default :
-                    //        Console.WriteLine("DANNY IS UNKNOWN");
-                    //        break;
-
-                    //}
-
-                    //for (int x = ages.Length - 1; x >= 0; x--)
-                    //{
-                    //    int ageValue = ages[x];
-                    //    Console.WriteLine(ageValue);
-                    //}
-
-                    //bool keepgoing = true;
-                    //int current = 0;
-                    //while (keepgoing)
-                    //{
-                    //    current += 1;
-                    //    if (current == 100001)
-                    //    {
-                    //        keepgoing = false;
-                    //        Console.WriteLine("WOOT");
-                    //    }
-                    //    else
-                    //    {
-                    //        Console.WriteLine(current);
-                    //    }
-
-                    //}
+            //    if (!isFound)
+            //    {
+            //        Console.WriteLine("Distinct Value:{0}", v);
+            //        int[] newArray = new int[printedValues.Length + 1];
+            //        printedValues.CopyTo(newArray, 0);
+            //        newArray[newArray.Length - 1] = v;
+            //        printedValues = newArray;
 
 
 
-                    //Console.WriteLine("{0}", isvalid);
-
-                    //Person Mark = new Person();
-                    //Mark.Age = 12;
-                    //Mark.BirthDate = DateTime.Now;
-
-                    //Person Danny = new Person();
-                    //Danny.Age = 13;
-                    //Danny.BirthDate = DateTime.Now;
-
-                    //int NewAge = Mark.Age + Danny.Age;
-
-                    //Console.WriteLine(NewAge);
 
 
-                    Console.Read();
+            //if (ismarkolder && whiteness)
+            //{
+            //    Console.WriteLine("valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid");
+            //}
+
+            //switch (dannyage)
+            //{
+            //    case 40:
+            //        Console.WriteLine("Danny is 38");
+            //        break;
+            //    default :
+            //        Console.WriteLine("DANNY IS UNKNOWN");
+            //        break;
+
+            //}
+
+            //for (int x = ages.Length - 1; x >= 0; x--)
+            //{
+            //    int ageValue = ages[x];
+            //    Console.WriteLine(ageValue);
+            //}
+
+            //bool keepgoing = true;
+            //int current = 0;
+            //while (keepgoing)
+            //{
+            //    current += 1;
+            //    if (current == 100001)
+            //    {
+            //        keepgoing = false;
+            //        Console.WriteLine("WOOT");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(current);
+            //    }
+
+            //}
 
 
 
-                }
-            }
+            //Console.WriteLine("{0}", isvalid);
+
+            //Person Mark = new Person();
+            //Mark.Age = 12;
+            //Mark.BirthDate = DateTime.Now;
+
+            //Person Danny = new Person();
+            //Danny.Age = 13;
+            //Danny.BirthDate = DateTime.Now;
+
+            //int NewAge = Mark.Age + Danny.Age;
+
+            //Console.WriteLine(NewAge);
+
+
+            Console.Read();
+
+
+
+            
         }
     }
 }
+
